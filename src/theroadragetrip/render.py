d252aa6edafbde17eaad2274022e9a63fdb1bb23
Esa Mehtätalo <emehtata@users.noreply.github.com>
2026-09-05 23:22:08 +0300

    Release version 0.9.0alpha

--- src/theroadragetrip/render.py
+++ src/theroadragetrip/render.py
@@ -382,7 +382,7 @@ def _get_game_version() -> str:
     try:
         return f"v{package_version('theroadragetrip')}"
     except PackageNotFoundError:
-        return "v0.8.0beta"
+        return "v0.9.0alpha"
 
 
 GAME_VERSION = _get_game_version()
